4c28d9b1ad0ca8f6d0d436e7b1b1b6dda628d311
seeds-anakai <a.nakai@seeds-std.co.jp>
2022-06-22 21:45:30 +0900

    add schedule function

--- srv/app.ts
+++ srv/app.ts
@@ -1,8 +1,20 @@
+// Node.js Core Modules
+import path from 'path';
+
 // AWS CDK
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { App, Duration, Stack, StackProps } from 'aws-cdk-lib';
 
 // AWS CDK - DynamoDB
 import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
+
+// AWS CDK - EventBridge
+import { Rule, Schedule } from 'aws-cdk-lib/aws-events';
+
+// AWS CDK - EventBridge - Event Targets
+import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
+
+// AWS CDK - Lambda
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 
 // AWS CDK - Systems Manager
 import { StringParameter } from 'aws-cdk-lib/aws-ssm';
@@ -59,6 +71,41 @@ class SlackReminderStack extends Stack {
       },
       readCapacity: 1,
       writeCapacity: 1,
+    });
+
+    // Schedule Function
+    const scheduleFunction = new Function(this, 'ScheduleFunction', {
+      runtime: Runtime.PYTHON_3_9,
+      code: Code.fromAsset(path.join(__dirname, 'schedule')),
+      handler: 'lambda_function.lambda_handler',
+      timeout: Duration.seconds(90),
+      environment: {
+        TZ: 'Asia/Tokyo',
+      },
+    });
+
+    // Schedule Rule
+    new Rule(this, 'ScheduleRule', {
+      schedule: Schedule.cron({
+        minute: '0',
+        hour: '15',
+        weekDay: 'SUN-THU',
+      }),
+      targets: [
+        new LambdaFunction(scheduleFunction),
+      ],
+    });
+
+    // Environment Variables
+    [['SETTING_PARAMETER_NAME', settingParameter.parameterName], ['EVENT_TABLE_NAME', eventTable.tableName]].forEach(([key, value]) => {
+      scheduleFunction.addEnvironment(key, value);
+    });
+
+    // Permissions
+    [scheduleFunction].forEach((grantee) => {
+      settingParameter.grantRead(grantee);
+      settingParameter.grantWrite(grantee);
+      eventTable.grantFullAccess(grantee);
     });
   }
 }
